69b8d179467ff20aed4da678f8323b625cdec8b6
tongifer <tpow@sas.upenn.edu>
2018-04-30 14:51:23 -0400

    add

--- project.py
+++ project.py
@@ -1,5 +1,12 @@
 # Import Statements
 import re
+import nltk
+import string
+from profanity import profanity
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from collections import Counter
 
 
 # Clustering Class
@@ -13,11 +20,12 @@ class Clustering(object):
 # 20858 Comments
 def parse_data(path):
     data = []
+    classes = {}  # Dictionary of classes in the form of {(Last name, first name, classID, term): Comment}
     with open(path) as f:
         for line in f:
             nametag = line.find('\"')
             nametag2 = line.find('\"', 1, len(line))
-            name = line[nametag+1:nametag2]
+            name = line[nametag + 1:nametag2]
             if nametag == 0 and nametag2 != -1 and name.isupper():
                 # Split name into LN, FN+MI
                 comment = name.split(",")
@@ -26,21 +34,74 @@ def parse_data(path):
                 comment = comment + rest[:2]
                 words = rest[2]
                 words = re.sub(r'[^\w\s\']', ' ', words.lower()).split()
-                comment = comment + words
-                data.append(comment)
+                words = tokenize(words)
+                if (len(words) > 0):
+                    # Populate classes dictionary
+                    curr_key = (comment[0], comment[1], comment[2], comment[3])
+                    classes[curr_key] = words
+                    # Populate data
+                    comment = comment + words
+                    data.append(comment)
             else:
                 comment = data[len(data) - 1]
                 # add words to this comment
                 words = re.sub(r'[^\w\s\']', ' ', line.lower()).split()
-                comment = comment + words
-                data[len(data) - 1] = comment
+                words = tokenize(words)
 
-    return data
+                if (len(words) > 0):
+                    # Populate classes dictionary
+                    curr_key = (comment[0], comment[1], comment[2], comment[3])
+                    classes[curr_key] = words
+                    # Populate data
+                    comment = comment + words
+                    data[len(data) - 1] = comment
+
+    return data, classes
+
+
+def get_count(classes):
+    term_frequencies = {}
+    for key, value in classes.items():
+        counts = dict(Counter(value))  # vect.fit_transform(value)
+        term_frequencies[key] = counts
+    return term_frequencies
 
 
 
+# Stopwords and lemmatizer
+nltk.download('wordnet')
+nltk.download('stopwords')
+stops = stopwords.words('english')
+stops.append('ive')
+stops.append('im')
+stops.append('etc')
+lmtzr = WordNetLemmatizer()
+
+
+def tokenize(words):
+    # rid of \n and \t
+    words = [x.strip('\t') for x in words]
+    # make words lowercase
+    words = [x.lower() for x in words]
+    # remove stopwords
+    words = [x for x in words if x not in stops]
+    # rid of punctuation
+    words = [''.join(x for x in s if x not in string.punctuation) for s in words]
+    # remove stopwords again
+    words = [x for x in words if x not in stops]
+    # rid of '' again
+    words = [x for x in words if x]
+    # rid of digits
+    words = [x for x in words if not any(c.isdigit() for c in x)]
+    words = [x for x in words if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+    words = [lmtzr.lemmatize(x) for x in words]
+    words = [x for x in words if len(x) > 2]
+    return words
 
 
 if __name__ == '__main__':
-    data = parse_data("./comments.csv")
+    data, classes = parse_data("./comments.csv")
     print(len(data))
+
+# print(len(classes))
+get_count(classes)
